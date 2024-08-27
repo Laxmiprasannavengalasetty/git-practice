928cac1ebf80bc0f6713ac9d048f8b968f87f784
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-08-27 10:33:22 +1000

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+PERSON1= ram
+PERSON2= sam
+
+echo "$PERSON1:: Hi {$PERSON2}, How are you?"
+echo "{$PERSON2}:: HELLO $PERSON1. I an going well"
+echo "{$PERSON1}:: Hi $PERSON2 . Nice to meet you"
