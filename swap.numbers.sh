a77f5b8621a7d2095eb58ac2e3da22fa02335816
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-08-30 08:05:34 +1000

    add

--- swap.numbers.sh
+++ swap.numbers.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ];
+
+    then
+    echo "Usage: $0 NUMBER1 NUMBER2"
+    exit 1
+fi
+
+
 NUMBER1=$1
 NUMBER2=$2
 
 echo "before swaping a=$NUMBER1 and b=$NUMBER2" 
 
-a=$(($NUMBER1 + $NUMBER2))
-b=$(($NUMBER1 - $NUMBER2))
-a=$(($NUMBER1 - $NUMBER2))
+NUMBER1=$(($NUMBER1 + $NUMBER2))
+NUMBER2=$(($NUMBER1 - $NUMBER2))
+NUMBER1=$(($NUMBER1 - $NUMBER2))
 
 echo "After swap a=$a and b=$b"
