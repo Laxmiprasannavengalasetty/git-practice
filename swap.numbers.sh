51c9dbcb0285dc9dec100b9d9cefc06ee95dff3c
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-08-30 08:01:39 +1000

    add

--- swap.numbers.sh
+++ swap.numbers.sh
@@ -5,8 +5,8 @@ NUMBER2=$2
 
 echo "before swaping a=$NUMBER1 and b=$NUMBER2" 
 
-a=$(($NUMBER1+$NUMBER2))
-b=$(($NUMBER1-$NUMBER2))
-a=$(($NUMBER1-$NUMBER2))
+a=$(($NUMBER1 + $NUMBER2))
+b=$(($NUMBER1 - $NUMBER2))
+a=$(($NUMBER1 - $NUMBER2))
 
 echo "After swap a=$a and b=$b"
