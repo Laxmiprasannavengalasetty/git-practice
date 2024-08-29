9416814ba2880547a07fe74403da8b471dda75b4
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-08-30 08:09:38 +1000

    add

--- swap.numbers.sh
+++ swap.numbers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# if [ "$#" -ne 2 ];
+if [ "$#" -ne 2 ]; # validating numbers
 
-#     then
-#     echo "Usage: $0 NUMBER1 NUMBER2"
-#     exit 1
-# fi
+    then
+    echo "Usage: only enter 2 numbers"
+    exit 1
+fi
 
 
 NUMBER1=$1
