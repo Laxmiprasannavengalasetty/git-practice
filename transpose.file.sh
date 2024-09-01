0a0d7a49d1650da05773cdc2b3b0f9bf3daea96f
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-01 14:03:54 +1000

    transpose

--- transpose.file.sh
+++ transpose.file.sh
@@ -32,7 +32,7 @@ awk '
 }
 END {
     # Print transposed data
-     print Transposed data:
+      Print Transposed data:
     for (j=1; j<=max_fields; j++) {
         str = a[1, j]
         for (i=2; i<=NR; i++) {
