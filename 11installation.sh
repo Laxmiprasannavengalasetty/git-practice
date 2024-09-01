505e1a303fbff5d1268caff5f598e2155af6ee74
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-01 16:51:09 +1000

    change

--- 11installation.sh
+++ 11installation.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)  # it provides 0 if it has root access
 echo "User ID is : $USERID"
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
  then 
  echo "please run this script with root user"
  exit 1
