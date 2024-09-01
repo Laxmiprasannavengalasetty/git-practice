5a51e4750733eecfbacaf552ba33ed01a00383cd
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-01 15:22:10 +1000

    transpose

---
+++ find-replace-string.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then  # 1. directory 2. find string 3. replace string
+
+   echo "Usage: <directory> <find string> <replace string>"
+    exit 1;
+fi
+
+directory="$1"
+search_string="$2"
+replace_string="$3"
+
+if [ ! -d "$directory" ]; then
+ echo " Directory not found"
+ exit 1;
+
+fi
+
+# iterate over the all .txt files in that directory
+for file in "$directory"/*.txt;
+ do
+   if [ -f "$file"]; then
+    echo "processing file: $file"
+    sed -i "s/${search_string}/${replace_string}/g" "$file"
+   fi
+done
+echo "Replacement completed"
