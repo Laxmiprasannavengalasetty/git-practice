8e5d243a11153850e0cd62a1b018f3d2512a9ee1
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-01 17:09:28 +1000

    change

--- 11installation.sh
+++ 11installation.sh
@@ -2,10 +2,29 @@
 
 USERID=$(id -u)  # it provides 0 if it has root access
 echo "User ID is : $USERID"
+
 if [ $USERID -ne 0 ]
  then 
  echo "please run this script with root user"
  exit 1
- fi
+
+fi
  
- ls -l
+ dnf list installed gittt
+
+if [ $? -ne 0 ] 
+  then
+       echo " git is not installed, going to install it:  "
+       dnf install git -y
+       if [ $? -ne 0 ] 
+          then
+          echo "git installation is not success.. check it"
+          exit 1:
+          else
+          echo " git installation success"
+        fi 
+    else
+    echo "Git is already installed, nothing to do.."
+
+fi
+
