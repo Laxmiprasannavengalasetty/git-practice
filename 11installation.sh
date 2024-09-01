c2cf1008674760c3df373bd0950f43777f3041ca
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-01 16:50:40 +1000

    change

--- 11installation.sh
+++ 11installation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-dnf install git -y
+USERID=$(id -u)  # it provides 0 if it has root access
+echo "User ID is : $USERID"
+if [ $USERID -ne 0]
+ then 
+ echo "please run this script with root user"
+ exit 1
+ fi
+ 
+ ls -l
